c1dbbc6db76f55f33d04c0736c2ce20a7215cc7e
DonizetiJr <dcs.junior22@gmail.com>
2017-02-10 13:13:48 -0200

    Deploy to Heroku

---
+++ config/express.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+    load = require('express-load'),
+    bodyParser = require('body-parser'),
+    expressValidator = require('express-validator');
+
+module.exports = function() {
+
+  var app = express();
+
+  app.use(express.static('./app/public'));
+  app.set('view engine','ejs');
+  app.set('views', './app/views');
+
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(expressValidator());
+
+
+  load('routes', {cwd: 'app'})
+      .then('infra')
+      .into(app);
+
+  app.use(function(req,res,next) {
+    res.status(404).render('erros/404');
+    next();
+  });
+
+  app.use(function(error,req,res,next) {
+    if (process.env.NODE_ENV == 'production') {
+      res.status(500).render('erros/500');
+      return;
+    }
+    next(error);
+  });
+
+  return app;
+}
